411d9c3b41f6371dccbe0a4e58535f5bdef0fe4d
c1324540 ITALO CARVALHO COSTA <c1324540@interno.bb.com.br>
2023-10-07 22:42:25 -0300

    init

---
+++ schemas/user_schema.py
@@ -0,0 +1,30 @@
+from typing import Optional, List
+from pydantic import BaseModel, EmailStr
+from schemas.article_schema import ArticleSchema
+
+
+class UserSchemaBase(BaseModel):
+    id: Optional[int] = None
+    name: str
+    last_name: str
+    email: EmailStr
+    is_admin: bool = False
+
+    class Config:
+        orm_mode: True
+
+
+class UserSchemaCreate(UserSchemaBase):
+    passwd: str
+
+
+class UserSchemaArticles(UserSchemaBase):
+    articles: Optional[List[ArticleSchema]]
+
+
+class UserSchemaUp(UserSchemaBase):
+    name: Optional[str]
+    last_name: Optional[str]
+    email: Optional[EmailStr]
+    passwd: Optional[str]
+    is_admin: Optional[bool]
